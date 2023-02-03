6ef19b2d98561a84a316aa3bd1dff5c2987696fe
DaviesHub <davieschuks.a@gmail.com>
2023-02-03 22:19:59 +0200

    Add urls for user_auth app

---
+++ user_auth/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'user_auth'
+urlpatterns = [
+    path('', views.user_login, name='user_login'),
+    path('register/', views.register, name='register'),
+    path('authenticate_user/', views.authenticate_user, name='authenticate_user'),
+    path('home/', views.home, name='home'),
+]
