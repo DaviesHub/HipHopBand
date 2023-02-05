e59c12f1de6640efa48d94158b0c935f65b01f4e
DaviesHub <davieschuks.a@gmail.com>
2023-02-05 21:50:31 +0200

    Add album model

--- bandapp/models.py
+++ bandapp/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Album(models.model):
-    cover_art = models.ImageField(upload_to='band_logos/', blank=True, null=True) 
+    title = models.CharField(max_length=255)
+    cover_art = models.ImageField(upload_to='band_logos/', blank=True, null=True)
+    release_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-release_date')
+
+    def __str__(self):
+        return self.title
