2dbeb86ee1d7a9a7cd3a1e31cda62021a19a0f67
DaviesHub <davieschuks.a@gmail.com>
2023-02-05 22:40:33 +0200

    Add slug field to models

--- bandapp/models.py
+++ bandapp/models.py
@@ -5,6 +5,7 @@ class Album(models.model):
     title = models.CharField(max_length=255)
     cover_art = models.ImageField(upload_to='band_logos/', blank=True, null=True)
     release_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
 
     class Meta:
         ordering = ('-release_date')
@@ -15,4 +16,5 @@ class Album(models.model):
 class TrackList(models.Model):
     album = models.ForeignKey(Album, related_name='track_list', on_delete=models.CASCADE)
     song_name = models.CharField(max_length=100)
-    track_id = models.IntegerField(primary_key=True)
+    track_id = models.IntegerField()
+    slug = models.SlugField()
