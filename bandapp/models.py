ff0535f72d465e6a44d2e2395322452812078d20
DaviesHub <davieschuks.a@gmail.com>
2023-02-06 23:09:14 +0200

    Add merch model and write string repr of models

--- bandapp/models.py
+++ bandapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Album(models.model):
+class Album(models.Model):
     title = models.CharField(max_length=255)
     cover_art = models.ImageField(upload_to='band_logos/', blank=True, null=True)
     release_date = models.DateTimeField(auto_now_add=True)
@@ -14,18 +14,39 @@ class Album(models.model):
         return self.title
 
 class TrackList(models.Model):
-    album = models.ForeignKey(Album, related_name='track_list', on_delete=models.CASCADE)
+    album_title = models.ForeignKey(Album, related_name='track_list', on_delete=models.CASCADE)
     song_name = models.CharField(max_length=100)
     track_id = models.IntegerField()
     slug = models.SlugField()
 
-class Tour(models.model):
+    def __str__(self):
+        return self.album_title
+
+class Tour(models.Model):
     tour_name = models.CharField(max_length=100)
     tour_date = models.DateTimeField()
     tour_location = models.CharField(max_length=255)
     slug = models.SlugField()
 
-class Members(models.model):
+    class Meta:
+        ordering = ('-tour_date')
+
+    def __str__(self):
+        return self.tour_name
+
+class Members(models.Model):
     member_photo = models.ImageField(upload_to='member_photos/', blank=True, null=True)
     member_name = models.CharField(max_length=100)
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.member_name
+
+class Merch(models.Model):
+    merch_photo = models.ImageField(upload_to='merch_photos/', blank=True, null=True)
+    merch_name = models.CharField(max_length=255)
+    merch_description = models.TextField(blank=True)
+    merch_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.member_name
