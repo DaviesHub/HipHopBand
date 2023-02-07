0305d902ee0e117f352d8fc77fd4be547897c65f
DaviesHub <davieschuks.a@gmail.com>
2023-02-07 10:40:41 +0200

    Rrgister models

--- bandapp/admin.py
+++ bandapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Album, TrackList, Tour, Members, Merch
 
 # Register your models here.
+admin.site.register(Album)
+admin.site.register(TrackList)
+admin.site.register(Tour)
+admin.site.register(Members)
+admin.site.register(Merch)
