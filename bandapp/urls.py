2f94a8ba975c42f7c96587592ae53c361ed2679a
DaviesHub <davieschuks.a@gmail.com>
2023-02-07 14:10:15 +0200

    Add other model views

--- bandapp/urls.py
+++ bandapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-from . import views
-
 urlpatterns = [
     path('<slug:album_slug>/', views.album_detail, name='album_detail'),
+    path('<slug:tour_slug>/', views.tour_detail, name='tour_detail'),
+    path('<slug:member_slug>/', views.member_detail, name='member_detail'),
+    path('<slug:merch_slug>/', views.merch_shop, name='merch_shop'),
 ]
--- bandapp/views.py
+++ bandapp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Album
+from .models import Album, Tour, Members, Merch
 
 # Create your views here.
 def album_detail(request, album_slug):
     album = get_object_or_404(Album, slug=album_slug)
+
+    return render(request, 'bandapp/album_detail.html', {'album': album})
+
+def tour_detail(request, tour_slug):
+    tour = get_object_or_404(Tour, slug=tour_slug)
+
+    return render(request, 'bandapp/album_detail.html', {'tour': tour})
+
+def member_detail(request, member_slug):
+    member = get_object_or_404(Members, slug=member_slug)
+
+    return render(request, 'bandapp/member_detail.html', {'member': member})
+
+def merch_shop(request, merch_slug):
+    merch = get_object_or_404(Merch, slug=merch_slug)
+
+    return render(request, 'bandapp/merch_detail.html', {'merch': merch})
