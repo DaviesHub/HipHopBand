d5b78a4275c6fad30801e52d872dcb80f1851ba0
DaviesHub <davieschuks.a@gmail.com>
2023-02-12 20:26:13 +0200

    Adjust urls

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -5,6 +5,7 @@ app_name = 'user_auth'
 urlpatterns = [
     #path('', views.user_login, name='user_login'),
     path('', views.index, name='index'),
+    path('login/', views.user_login, name='user_login'),
     path('register/', views.register, name='register'),
     path('authenticate_user/', views.authenticate_user, name='authenticate_user'),
     #path('home/', views.home, name='home'),
