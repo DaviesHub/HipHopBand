d88e026600ad45aa2c58037e4a5c79517d3381d3
DaviesHub <davieschuks.a@gmail.com>
2023-03-16 00:58:32 +0200

    Add docstring for each view function in main app

--- bandapp/views.py
+++ bandapp/views.py
@@ -8,6 +8,19 @@ from .models import Album, Tour, Merch, Ticket, TrackList, Track
 
 # Create your views here.
 def albums(request):
+    '''
+        Renders a list of albums by the hip hop band.
+
+        Args:
+            request: A HttpRequest object that contains the album request parameters and user information.
+
+        Returns:
+            A HttpResponse object that renders an HTML template with a list of album objects from the database.
+
+        Raises:
+            Http404: If the requested album does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         album_list = Album.objects.order_by('-release_date')
         context = {'album_list': album_list,}
@@ -18,7 +31,22 @@ def albums(request):
             reverse('user_auth:user_login')
         )
 
+
 def tracklist(request, album_slug):
+    '''
+        Renders a list of tracks belonging to an album.
+
+        Args:
+            request: A HttpRequest object that contains the track request parameters and user information.
+            album_slug: A string that represents the album slug.
+
+        Returns:
+            A HttpResponse object that renders an HTML template with a list of an album track objects from the database.
+
+        Raises:
+            Http404: If the requested album or track does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         album = get_object_or_404(Album, slug=album_slug)
         tracklist = get_object_or_404(TrackList, album=album)
@@ -31,6 +59,19 @@ def tracklist(request, album_slug):
         )
 
 def tours(request):
+    '''
+        Renders a list of available tours by the hip hop band.
+
+        Args:
+            request: A HttpRequest object that contains the tour request parameters and user information.
+
+        Returns:
+            A HttpResponse object that renders an HTML template with a list of tour objects from the database.
+
+        Raises:
+            Http404: If the requested tour does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         tour_list = Tour.objects.order_by('-tour_date')[:]
         context = {'tour_list': tour_list,}
@@ -41,7 +82,23 @@ def tours(request):
             reverse('user_auth:user_login')
         )
 
+
 def buy_ticket(request, slug):
+    '''
+        Renders a form that enables users choose number of tickets to buy for a tour.
+
+        Args:
+            request: A HttpRequest object that contains the tour request parameters and user information.
+            slug: A string that represents the unique identifier for a tour object.
+
+        Returns:
+            A HttpResponseRedirect object that redirects the user to the ticket confirmation page on successful purchase.
+            A rendered HTML template with a list of tours if there was an error in the form submission or the user is not authenticated.
+
+        Raises:
+            Http404: If the requested tour does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         tour = get_object_or_404(Tour, slug=slug)
         tour_list = Tour.objects.order_by('-tour_date')[:]
@@ -66,7 +123,22 @@ def buy_ticket(request, slug):
             reverse('user_auth:user_login') 
         )
 
+
 def ticket_confirmation(request, ticket_id):
+    '''
+        View function that confirms successful ticket purchase to user.
+
+        Args:
+            request: A HttpRequest object that contains the tour and ticket request parameters and user information.
+            ticket_id: An integer that represents the unique identifier for a ticket object.
+
+        Returns:
+            a page showing the user the number of tickers purchased.
+
+        Raises:
+            Http404: If the requested ticket does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         ticket = get_object_or_404(Ticket, id=ticket_id, user=request.user)
         tour = ticket.tour
@@ -78,6 +150,19 @@ def ticket_confirmation(request, ticket_id):
         )
 
 def merch_shop(request):
+    '''
+        Renders a list of merch sold by the hip hop band.
+
+        Args:
+            request: A HttpRequest object that contains the merch request parameters and user information.
+
+        Returns:
+            A HttpResponse object that renders an HTML template with a list of merch objects from the database.
+
+        Raises:
+            Http404: If the requested merch does not exist in the database.
+    '''
+
     if request.user.is_authenticated:
         merch_list = Merch.objects.all()
         context = {'merch_list': merch_list}
@@ -88,7 +173,20 @@ def merch_shop(request):
             reverse('user_auth:user_login')
         )
 
+
 def merch_search(request):
+    '''
+        merch_search function enables authenticated users to search for available merch based on a given query.
+
+        Args:
+        request (HttpRequest): An instance of the HttpRequest object that contains metadata about the user's request.
+
+        Returns:
+        It returns the rendered template for merch objects that match and query context variables.
+
+        Raises:
+        N/A
+'''
     if request.user.is_authenticated:
         query = request.GET.get('query', '')
         merch_list = Merch.objects.filter(Q(merch_description__icontains=query) | Q(merch_name__icontains=query))
@@ -99,5 +197,18 @@ def merch_search(request):
             reverse('user_auth:user_login')
         )
 
+
 def contact(request):
+    '''
+        Renders the hip hop band's contacts.
+
+        Args:
+            request: A HttpRequest object that contains the request parameters.
+
+        Returns:
+            A HttpResponse object that renders an HTML template with the band's contacts.
+
+        Raises:
+            N/A
+    '''
     return render(request, 'bandapp/contact.html')
